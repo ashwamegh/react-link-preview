a8f1f3387193eb4a6ab09ce033b2d350afbb48a7
Shashank Shekhar <shashank7200@gmail.com>
2020-05-09 19:48:35 +0530

    commiting example directory

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import LinkPreview from 'react-link-preview'
+import 'react-link-preview/dist/index.css'
+
+const App = () => {
+  return <LinkPreview url="https://reactjs.org" />
+}
+
+export default App
