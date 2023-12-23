091b3a40837a7c2b1bf3f03e03aec4a550b4392c
shashank7200 <shashank7200@gmail.com>
2023-12-23 23:35:14 +0530

    updated example

--- example/src/App.js
+++ example/src/App.js
@@ -3,21 +3,26 @@ import React from 'react'
 import LinkPreview from '@ashwamegh/react-link-preview'
 import '@ashwamegh/react-link-preview/dist/index.css'
 
-function CustomComponent ({ loading, preview }) {
-	return loading 
-	? (<h1>Loading...</h1>)
-	: (
-		<div>
-			<p>Domain: { preview.domain }</p>
-			<p>Title: { preview.title }</p>
-			<p>Description: { preview.description }</p>
-			<img height="100px" width="100px" src={preview.img} alt={preview.title} />
-		</div>
-	)
+function CustomComponent({ loading, preview, onClick }) {
+  return loading ? (
+    <h1>Loading...</h1>
+  ) : (
+    <div>
+      <p>Domain: {preview.domain}</p>
+      <p>Title: {preview.title}</p>
+      <p>Description: {preview.description}</p>
+      <img height='100px' width='100px' src={preview.img} alt={preview.title} />
+    </div>
+  )
 }
 
-function App () {
-  return <LinkPreview url="https://reactjs.org"/>
+function App() {
+  return (
+    <LinkPreview
+      url='https://reactjs.org'
+      customDomain='https://lpdg-server.azurewebsites.net/parse/link'
+    />
+  )
 }
 
 export default App
